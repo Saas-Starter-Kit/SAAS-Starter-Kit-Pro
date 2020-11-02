779a6735bcae072670e0e13673b3f60f988219d2
iqbal125 <iqbal125@yahoo.com>
2020-11-02 14:04:28 -0800

    added tailwind ui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
+}
