f9dcca413f6cea8ca1b953d8e65b2369dfe1a9eb
iqbal125 <iqbal125@yahoo.com>
2020-11-03 16:28:00 -0800

    add illustration to landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px'
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/ui')({
-      layout: 'sidebar',
-    }),
+      layout: 'sidebar'
+    })
   ]
-}
+};
