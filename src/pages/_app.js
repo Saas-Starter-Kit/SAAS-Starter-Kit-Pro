85348bf3c899927318196958bccb815fa6a807dc
iqbal125 <iqbal125@yahoo.com>
2020-11-04 14:41:18 -0800

    added style components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../utils/styledComponentsTheme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
