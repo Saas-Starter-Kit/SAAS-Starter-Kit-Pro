b65f1a00269848c837639f55e362a3f88dc2b642
iqbal125 <iqbal125@yahoo.com>
2020-11-05 14:05:18 -0800

    fix footer

--- src/components/marketing/Layout/footer.js
+++ src/components/marketing/Layout/footer.js
@@ -1,7 +1,73 @@
 import styled from 'styled-components';
+import Link from 'next/link';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  padding: 2rem;
+  background-color: #1e429f;
+  background-color: rgba(30, 66, 159, 1);
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FooterLink = styled.p`
+  padding: 1rem;
+  font-weight: 800;
+  color: white;
+  cursor: pointer;
+`;
+
+const IMGContainer = styled.div`
+  background-color: #5145cd;
+  background-color: rgba(81, 69, 205, 1);
+`;
+
+const BottomFooter = styled.div`
+  background-color: #233876;
+  color: white;
+  font-size: 1.25rem;
+  padding: 1rem;
+  text-align: center;
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='bg-blue-900'>
+      <IMGContainer>
+        <img src='/illustrations/waveFooter.svg' alt='wave' />
+      </IMGContainer>
+      <Container>
+        <Column>
+          <Link href='/'>
+            <FooterLink>Link1</FooterLink>
+          </Link>
+          <Link href='/'>
+            <FooterLink>Link2</FooterLink>
+          </Link>
+          <Link href='/'>
+            <FooterLink>Link3</FooterLink>
+          </Link>
+        </Column>
+        <Column>
+          <Link href='/'>
+            <FooterLink>Link1</FooterLink>
+          </Link>
+          <Link href='/'>
+            <FooterLink>Link2</FooterLink>
+          </Link>
+          <Link href='/'>
+            <FooterLink>Link3</FooterLink>
+          </Link>
+        </Column>
+      </Container>
+      <BottomFooter>Copyright &copy; 2020 Example Inc</BottomFooter>
+    </div>
+  );
 };
 
 export default Footer;
