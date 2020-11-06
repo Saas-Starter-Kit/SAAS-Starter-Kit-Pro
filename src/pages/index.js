69bb2f9a1146f37b9fcb4602ba889e14e24830dd
iqbal125 <iqbal125@yahoo.com>
2020-11-06 14:13:54 -0800

    added testimonial

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import LandingFeature from '../components/marketing/LandingPage/landingFeatures'
 import HeroSection from '../components/marketing/LandingPage/heroSection';
 import CTASection from '../components/marketing/LandingPage/ctaSection';
 import TripleColFeatures from '../components/marketing/LandingPage/tripleColFeatures';
+import Testimonial from '../components/marketing/LandingPage/testimonial';
 import Layout from '../components/marketing/Layout';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <Layout>
       <HeroSection />
       <TripleColFeatures />
+      <Testimonial />
       <LandingFeature />
       <CTASection />
     </Layout>
