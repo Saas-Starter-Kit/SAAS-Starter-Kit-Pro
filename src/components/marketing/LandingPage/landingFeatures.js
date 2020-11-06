5e1fc98527d124a70e29096150ccc2418dd09ea9
iqbal125 <iqbal125@yahoo.com>
2020-11-06 13:21:03 -0800

    added svg

--- src/components/marketing/LandingPage/landingFeatures.js
+++ src/components/marketing/LandingPage/landingFeatures.js
@@ -1,4 +1,11 @@
 // Feature Section for Landing Page
+import styled from 'styled-components';
+
+const MenuImg = styled.img`
+  height: 2rem;
+  width: 2rem;
+  color: white;
+`;
 
 const LandingFeatures = () => {
   return (
@@ -32,21 +39,10 @@ const LandingFeatures = () => {
           </defs>
           <rect width='404' height='784' fill='url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)' />
         </svg>
-
-        <div className='relative'>
-          <h3 className='text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10'>
-            A better way to send money
-          </h3>
-          <p className='mt-4 max-w-3xl mx-auto text-center text-xl leading-7 text-gray-500'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam voluptatum
-            cupiditate veritatis in, accusamus quisquam.
-          </p>
-        </div>
-
         <div className='relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center'>
           <div className='relative'>
             <h4 className='text-2xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9'>
-              Transfer funds world-wide
+              Awesome Feature about App 1
             </h4>
             <p className='mt-3 text-lg leading-7 text-gray-500'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi
@@ -59,27 +55,11 @@ const LandingFeatures = () => {
                 <div className='flex'>
                   <div className='flex-shrink-0'>
                     <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-                      {/*<!-- Heroicon name: globe-alt -->*/}
-                      <svg
-                        className='h-6 w-6'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                      >
-                        <path
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
-                          stroke-width='2'
-                          d='M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9'
-                        />
-                      </svg>
+                      <MenuImg src='/icons/chart-bar.svg' alt='chart bar' />
                     </div>
                   </div>
                   <div className='ml-4'>
-                    <h5 className='text-lg leading-6 font-medium text-gray-900'>
-                      Competitive exchange rates
-                    </h5>
+                    <h5 className='text-lg leading-6 font-medium text-gray-900'>Sub Feature 1</h5>
                     <p className='mt-2 text-base leading-6 text-gray-500'>
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
                       perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
@@ -91,57 +71,11 @@ const LandingFeatures = () => {
                 <div className='flex'>
                   <div className='flex-shrink-0'>
                     <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-                      {/*<!-- Heroicon name: scale -->*/}
-                      <svg
-                        className='h-6 w-6'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                      >
-                        <path
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
-                          stroke-width='2'
-                          d='M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3'
-                        />
-                      </svg>
+                      <MenuImg src='/icons/chart-bar.svg' alt='chart bar' />
                     </div>
                   </div>
                   <div className='ml-4'>
-                    <h5 className='text-lg leading-6 font-medium text-gray-900'>No hidden fees</h5>
-                    <p className='mt-2 text-base leading-6 text-gray-500'>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                      perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li className='mt-10'>
-                <div className='flex'>
-                  <div className='flex-shrink-0'>
-                    <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-                      {/*<!-- Heroicon name: lightning-bolt -->*/}
-                      <svg
-                        className='h-6 w-6'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                      >
-                        <path
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
-                          stroke-width='2'
-                          d='M13 10V3L4 14h7v7l9-11h-7z'
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                  <div className='ml-4'>
-                    <h5 className='text-lg leading-6 font-medium text-gray-900'>
-                      Transfers are instant
-                    </h5>
+                    <h5 className='text-lg leading-6 font-medium text-gray-900'>Sub Feature 2</h5>
                     <p className='mt-2 text-base leading-6 text-gray-500'>
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
                       perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
@@ -189,7 +123,6 @@ const LandingFeatures = () => {
             />
           </div>
         </div>
-
         <svg
           className='hidden lg:block absolute right-full transform translate-x-1/2 translate-y-12'
           width='404'
@@ -218,12 +151,11 @@ const LandingFeatures = () => {
           </defs>
           <rect width='404' height='784' fill='url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)' />
         </svg>
-
         <div className='relative mt-12 sm:mt-16 lg:mt-24'>
           <div className='lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center'>
             <div className='lg:col-start-2'>
               <h4 className='text-2xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9'>
-                Always in the loop
+                Awesome Feature about App 2
               </h4>
               <p className='mt-3 text-lg leading-7 text-gray-500'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ex obcaecati natus
@@ -235,27 +167,11 @@ const LandingFeatures = () => {
                   <div className='flex'>
                     <div className='flex-shrink-0'>
                       <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-                        {/*<!-- Heroicon name: annotation -->*/}
-                        <svg
-                          className='h-6 w-6'
-                          xmlns='http://www.w3.org/2000/svg'
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          stroke='currentColor'
-                        >
-                          <path
-                            stroke-linecap='round'
-                            stroke-linejoin='round'
-                            stroke-width='2'
-                            d='M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z'
-                          />
-                        </svg>
+                        <MenuImg src='/icons/chart-bar.svg' alt='chart bar' />
                       </div>
                     </div>
                     <div className='ml-4'>
-                      <h5 className='text-lg leading-6 font-medium text-gray-900'>
-                        Mobile notifications
-                      </h5>
+                      <h5 className='text-lg leading-6 font-medium text-gray-900'>Sub Feature 1</h5>
                       <p className='mt-2 text-base leading-6 text-gray-500'>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
                         perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
@@ -267,27 +183,11 @@ const LandingFeatures = () => {
                   <div className='flex'>
                     <div className='flex-shrink-0'>
                       <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
-                        {/*<!-- Heroicon name: mail -->*/}
-                        <svg
-                          className='h-6 w-6'
-                          xmlns='http://www.w3.org/2000/svg'
-                          fill='none'
-                          viewBox='0 0 24 24'
-                          stroke='currentColor'
-                        >
-                          <path
-                            stroke-linecap='round'
-                            stroke-linejoin='round'
-                            stroke-width='2'
-                            d='M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
-                          />
-                        </svg>
+                        <MenuImg src='/icons/chart-bar.svg' alt='chart bar' />
                       </div>
                     </div>
                     <div className='ml-4'>
-                      <h5 className='text-lg leading-6 font-medium text-gray-900'>
-                        Reminder emails
-                      </h5>
+                      <h5 className='text-lg leading-6 font-medium text-gray-900'>Sub Feature 2</h5>
                       <p className='mt-2 text-base leading-6 text-gray-500'>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
                         perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
