f5ec180ca589bcb7828fd378a0d44e11b53a6795
iqbal125 <iqbal125@yahoo.com>
2020-11-06 14:02:55 -0800

    added triple column sections

---
+++ src/components/marketing/LandingPage/tripleColFeatures.js
@@ -0,0 +1,61 @@
+//Triple column features
+
+const TripleColFeatures = () => {
+  return (
+    <div className='pt-12 bg-gray-50'>
+      <div className='pb-20'>
+        <h3 class='text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10'>
+          A better way to send money
+        </h3>
+        <p class='mt-4 max-w-3xl mx-auto text-center text-xl leading-7 text-gray-500'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus magnam voluptatum
+          cupiditate veritatis in, accusamus quisquam.
+        </p>
+      </div>
+      <div className='max-w-xl mx-auto px-4 sm:px-6 lg:max-w-screen-xl lg:px-8'>
+        <div className='lg:grid lg:grid-cols-3 lg:gap-8'>
+          <div>
+            <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
+              {/*<!-- Heroicon name: globe-alt -->*/}
+            </div>
+            <div className='mt-5'>
+              <h5 className='text-lg leading-6 font-medium text-gray-900'>
+                Competitive exchange rates
+              </h5>
+              <p className='mt-2 text-base leading-6 text-gray-500'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+          <div className='mt-10 lg:mt-0'>
+            <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
+              {/*<!-- Heroicon name: scale -->*/}
+            </div>
+            <div className='mt-5'>
+              <h5 className='text-lg leading-6 font-medium text-gray-900'>No hidden fees</h5>
+              <p className='mt-2 text-base leading-6 text-gray-500'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+          <div className='mt-10 lg:mt-0'>
+            <div className='flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white'>
+              {/*<!-- Heroicon name: lightning-bolt -->*/}
+            </div>
+            <div className='mt-5'>
+              <h5 className='text-lg leading-6 font-medium text-gray-900'>Transfers are instant</h5>
+              <p className='mt-2 text-base leading-6 text-gray-500'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TripleColFeatures;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@
 import LandingFeature from '../components/marketing/LandingPage/landingFeatures';
 import HeroSection from '../components/marketing/LandingPage/heroSection';
 import CTASection from '../components/marketing/LandingPage/ctaSection';
+import TripleColFeatures from '../components/marketing/LandingPage/tripleColFeatures';
 import Layout from '../components/marketing/Layout';
 
 export default function Home() {
   return (
     <Layout>
       <HeroSection />
+      <TripleColFeatures />
       <LandingFeature />
       <CTASection />
     </Layout>
