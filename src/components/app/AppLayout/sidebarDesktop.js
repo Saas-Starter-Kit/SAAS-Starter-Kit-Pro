400ada05e8e00a45f3fb50648d42bfa949bd10fd
iqbal125 <iqbal125@yahoo.com>
2020-11-06 17:45:01 -0800

    extracted out sidebars

---
+++ src/components/app/AppLayout/sidebarDesktop.js
@@ -0,0 +1,111 @@
+const SidebarDesktop = () => {
+  return (
+    <div className='hidden md:flex md:flex-shrink-0'>
+      <div className='flex flex-col w-64'>
+        {/*<!-- Sidebar component, swap this element with another sidebar if you like -->*/}
+        <div className='flex flex-col h-0 flex-1 bg-indigo-800'>
+          <div className='flex-1 flex flex-col pt-5 pb-4 overflow-y-auto'>
+            <div className='flex items-center flex-shrink-0 px-4'>
+              <img
+                className='h-8 w-auto'
+                src='https://tailwindui.com/img/logos/v1/workflow-logo-on-brand.svg'
+                alt='Workflow'
+              />
+            </div>
+            <nav className='mt-5 flex-1 px-2 bg-indigo-800 space-y-1'>
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md bg-indigo-900 focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: home -->*/}
+                <svg
+                  className='mr-3 h-6 w-6 text-indigo-400 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+                  />
+                </svg>
+                Dashboard
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: users -->*/}
+                <svg
+                  className='mr-3 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'
+                  />
+                </svg>
+                Team
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: folder -->*/}
+                <svg
+                  className='mr-3 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
+                  />
+                </svg>
+                Projects
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: calendar -->*/}
+                <svg
+                  className='mr-3 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
+                  />
+                </svg>
+                Calendar
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarDesktop;
---
+++ src/components/app/AppLayout/sidebarMobile.js
@@ -0,0 +1,156 @@
+const SidebarMobile = () => {
+  return (
+    <div className='md:hidden'>
+      <div className='fixed inset-0 flex z-40'>
+        {/*<!--
+        Off-canvas menu overlay, show/hide based on off-canvas menu state.
+
+        Entering: "transition-opacity ease-linear duration-300"
+          From: "opacity-0"
+          To: "opacity-100"
+        Leaving: "transition-opacity ease-linear duration-300"
+          From: "opacity-100"
+          To: "opacity-0"
+      -->*/}
+        <div className='fixed inset-0'>
+          <div className='absolute inset-0 bg-gray-600 opacity-75'></div>
+        </div>
+        {/*<!--
+        Off-canvas menu, show/hide based on off-canvas menu state.
+
+        Entering: "transition ease-in-out duration-300 transform"
+          From: "-translate-x-full"
+          To: "translate-x-0"
+        Leaving: "transition ease-in-out duration-300 transform"
+          From: "translate-x-0"
+          To: "-translate-x-full"
+      -->*/}
+        <div className='relative flex-1 flex flex-col max-w-xs w-full bg-indigo-800'>
+          <div className='absolute top-0 right-0 -mr-14 p-1'>
+            <button
+              className='flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600'
+              aria-label='Close sidebar'
+            >
+              <svg
+                className='h-6 w-6 text-white'
+                stroke='currentColor'
+                fill='none'
+                viewBox='0 0 24 24'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+          <div className='flex-1 h-0 pt-5 pb-4 overflow-y-auto'>
+            <div className='flex-shrink-0 flex items-center px-4'>
+              <img
+                className='h-8 w-auto'
+                src='https://tailwindui.com/img/logos/v1/workflow-logo-on-brand.svg'
+                alt='Workflow'
+              />
+            </div>
+            <nav className='mt-5 px-2 space-y-1'>
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-indigo-900 focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: home -->*/}
+                <svg
+                  className='mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+                  />
+                </svg>
+                Dashboard
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-indigo-300 hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: users -->*/}
+                <svg
+                  className='mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'
+                  />
+                </svg>
+                Team
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-indigo-300 hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: folder -->*/}
+                <svg
+                  className='mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
+                  />
+                </svg>
+                Projects
+              </a>
+
+              <a
+                href='#'
+                className='group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-indigo-300 hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'
+              >
+                {/*<!-- Heroicon name: calendar -->*/}
+                <svg
+                  className='mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
+                  />
+                </svg>
+                Calendar
+              </a>
+            </nav>
+          </div>
+        </div>
+        <div className='flex-shrink-0 w-14'>
+          {/*<!-- Force sidebar to shrink to fit close icon -->*/}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarMobile;
