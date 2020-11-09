847f83914c7326a7f4f890f006d912cb6b4f2cad
iqbal125 <iqbal125@yahoo.com>
2020-11-09 12:26:07 -0800

    change project structure

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,19 +1,6 @@
 //Landing Page
-import LandingFeature from '../components/marketing/LandingPage/landingFeatures';
-import HeroSection from '../components/marketing/LandingPage/heroSection';
-import CTASection from '../components/marketing/LandingPage/ctaSection';
-import TripleColFeatures from '../components/marketing/LandingPage/tripleColFeatures';
-import Testimonial from '../components/marketing/LandingPage/testimonial';
-import Layout from '../components/marketing/Layout';
+import Landing from '../screens/Marketing/LandingPage';
 
 export default function Home() {
-  return (
-    <Layout>
-      <HeroSection />
-      <TripleColFeatures />
-      <Testimonial />
-      <LandingFeature />
-      <CTASection />
-    </Layout>
-  );
+  return <Landing />;
 }
