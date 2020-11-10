8f7ea50caae5053094a063edaa4ae82d5a05d492
iqbal125 <iqbal125@yahoo.com>
2020-11-10 12:31:16 -0800

    change project structure

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 //Landing Page
-import Landing from '../screens/Marketing/LandingPage';
+import Landing from '../screens/Marketing/LandingPageHome';
 
 export default function Home() {
   return <Landing />;
