eb015275ab94ce607c75925e0ccc21f333984744
iqbal125 <iqbal125@yahoo.com>
2020-11-11 12:39:48 -0800

    fix tailwind production

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
   theme: {
     screens: {
       sm: '640px',
