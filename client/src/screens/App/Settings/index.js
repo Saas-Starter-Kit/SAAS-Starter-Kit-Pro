17a919611d3ac4b3dcef4f34a40151f84267e9c9
iqbal125 <iqbal125@yahoo.com>
2020-11-12 14:46:39 -0800

    add email change

--- client/src/screens/App/Settings/index.js
+++ client/src/screens/App/Settings/index.js
@@ -3,15 +3,17 @@ import AuthContext from '../../../utils/authContext';
 
 const Settings = () => {
   const { firebase, authState } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState(authState.user.email);
+  const [username, setUsername] = useState(authState.user.username);
   const curUser = firebase.auth().currentUser;
   const id = authState.user ? authState.user.id.user : null;
 
-  const updateEmail = () => {
+  const updateUsername = (event) => {
+    event.preventDefault();
+
     curUser
       .updateProfile({
-        displayName: 'RRRR'
+        displayName: username
       })
       .then(function () {
         // Update successful.
@@ -21,21 +23,50 @@ const Settings = () => {
       });
   };
 
+  const updateEmail = (event) => {
+    event.preventDefault();
+
+    curUser
+      .updateEmail(email)
+      .then((res) => {
+        // Update successful.
+        console.log('ffff');
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
   return (
     <div>
       <h1>Settings</h1>
+      <h3>Changing username and email will cause user to get logged out</h3>
       <div>
         <h2>Update Username</h2>
         <form>
-          <input type='text' />
-          <button onClick={updateEmail}>Save</button>
+          <label htmlFor='title' className='block text-sm font-medium leading-5 text-gray-700'>
+            Username:
+          </label>
+          <input onChange={handleUsernameChange} value={username} type='text' />
+          <button onClick={updateUsername}>Save</button>
         </form>
       </div>
       <div>
         <h2>Update Email</h2>
         <form>
-          <input type='text' />
-          <button>Save</button>
+          <label htmlFor='title' className='block text-sm font-medium leading-5 text-gray-700'>
+            Username:
+          </label>
+          <input type='email' onChange={handleEmailChange} value={email} type='text' />
+          <button onClick={updateEmail}>Save</button>
         </form>
       </div>
       {console.log(authState, firebase)}
