843564434bbfdeaa3eef4e16a2382f4c4e99ba07
iqbal125 <iqbal125@yahoo.com>
2020-11-13 18:22:59 -0800

    fix mobile menu

--- client/src/components/marketing/Navigation/mobileMenu.js
+++ client/src/components/marketing/Navigation/mobileMenu.js
@@ -13,14 +13,8 @@ const MobileMenu = ({ mobileMenuHandler }) => {
       <div className='rounded-lg shadow-lg'>
         <div className='rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50'>
           <div className='pt-5 pb-6 px-5 space-y-6'>
-            <div className='flex items-center justify-between'>
-              <div>
-                <img
-                  className='h-8 w-auto'
-                  src='https://tailwindui.com/img/logos/workflow-mark-on-white.svg'
-                  alt='Workflow'
-                />
-              </div>
+            <div className='flex items-center justify-between py-4'>
+              <img className='h-8 w-auto' src='/logo/small_logo.svg' alt='Workflow' />
               <div className='-mr-2'>
                 <button
                   onClick={mobileMenuHandler}
@@ -33,6 +27,29 @@ const MobileMenu = ({ mobileMenuHandler }) => {
             </div>
             <div>
               <nav className='grid grid-cols-1 gap-7'>
+                <Link href='/pricing'>
+                  <div
+                    href='#'
+                    className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
+                  >
+                    <div className='flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white'>
+                      {/*<!-- Heroicon name: view-grid -->*/}
+                      <MenuImg src='/icons/view-grid.svg' alt='click' />
+                    </div>
+                    <div className='text-base leading-6 font-medium text-gray-900'>Pricing</div>
+                  </div>
+                </Link>
+                <Link href='/app'>
+                  <div className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'>
+                    <div className='flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white'>
+                      {/*<!-- Heroicon name: view-grid -->*/}
+                      <MenuImg src='/icons/view-grid.svg' alt='click' />
+                    </div>
+                    <div className='text-base leading-6 font-medium text-gray-900'>App</div>
+                  </div>
+                </Link>
+                <hr />
+                <h2 className='text-lg'>Solutions: </h2>
                 <Link href='#'>
                   <div className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'>
                     <div className='flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white'>
@@ -80,51 +97,16 @@ const MobileMenu = ({ mobileMenuHandler }) => {
                     </div>
                   </div>
                 </Link>
-                <Link href='#'>
-                  <div
-                    href='#'
-                    className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                  >
-                    <div className='flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white'>
-                      {/*<!-- Heroicon name: view-grid -->*/}
-                      <MenuImg src='/icons/view-grid.svg' alt='click' />
-                    </div>
-                    <div className='text-base leading-6 font-medium text-gray-900'>Pricing</div>
-                  </div>
-                </Link>
-                <Link href='#'>
-                  <div
-                    href='#'
-                    className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                  >
-                    <div className='flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white'>
-                      {/*<!-- Heroicon name: view-grid -->*/}
-                      <MenuImg src='/icons/view-grid.svg' alt='click' />
-                    </div>
-                    <div className='text-base leading-6 font-medium text-gray-900'>About</div>
-                  </div>
-                </Link>
               </nav>
             </div>
           </div>
           <div className='py-6 px-5 space-y-6'>
             <div className='space-y-6'>
-              <span className='w-full flex rounded-md shadow-sm'>
-                <a
-                  href='#'
-                  className='w-full flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'
-                >
+              <Link href='/login'>
+                <div className='w-full flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
                   Sign up
-                </a>
-              </span>
-              <p className='text-center text-base leading-6 font-medium text-gray-500'>
-                <a
-                  href='#'
-                  className='text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150'
-                >
-                  Sign in
-                </a>
-              </p>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
