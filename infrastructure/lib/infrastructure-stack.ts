3e378fa2e61c31a01544989563fd53288f187c64
iqbal125 <iqbal125@yahoo.com>
2020-11-15 12:38:20 -0800

    remove env vars

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core"
 import * as ec2 from "@aws-cdk/aws-ec2"
 import * as ecs from "@aws-cdk/aws-ecs"
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns"
+import * as rds from "@aws-cdk/aws-rds"
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,9 +14,20 @@ export class InfrastructureStack extends cdk.Stack {
       natGateways: 1,
     })
 
+    /* 
+
+        Fargate Service
+
+    */
     const cluster = new ecs.Cluster(this, "Cluster", {
       vpc,
     })
+
+    const securityGroupFargate = new ec2.SecurityGroup(this, `security-group`, {
+      vpc,
+    })
+
+    securityGroupFargate.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80))
 
     const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
       cluster: cluster,
@@ -23,10 +35,34 @@ export class InfrastructureStack extends cdk.Stack {
       taskImageOptions: {image: ecs.ContainerImage.fromAsset("../server")},
       memoryLimitMiB: 2048,
       publicLoadBalancer: true,
+      securityGroups: [securityGroupFargate],
     })
 
     loadBalancedFargateService.targetGroup.configureHealthCheck({
       path: "/health",
     })
+
+    /* 
+
+    RDS DATABASE SETUP
+
+    */
+
+    const db_name = "postgresDB"
+    const db_username = "admin"
+
+    const dbInstance = new rds.DatabaseInstance(this, "Instance", {
+      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_10}),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromGeneratedSecret(db_username),
+      databaseName: db_name,
+    })
+
+    dbInstance.connections.allowDefaultPortFromAnyIpv4()
+    dbInstance.connections.allowFrom(securityGroupFargate, ec2.Port.tcp(5432))
   }
 }
