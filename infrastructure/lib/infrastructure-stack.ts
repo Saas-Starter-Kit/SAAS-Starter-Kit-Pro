b29874b7fc3ee43660dbce340fdc883d577527b1
iqbal125 <iqbal125@yahoo.com>
2020-11-16 14:30:13 -0800

    add dynamic env vars

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -4,6 +4,9 @@ import * as ecs from "@aws-cdk/aws-ecs"
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns"
 import * as rds from "@aws-cdk/aws-rds"
 import * as secretsmanager from "@aws-cdk/aws-secretsmanager"
+import * as iam from "@aws-cdk/aws-iam"
+
+require("dotenv").config()
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -14,6 +17,91 @@ export class InfrastructureStack extends cdk.Stack {
       maxAzs: 3,
       natGateways: 1,
     })
+
+    /* 
+        Secrets Setup 
+
+    */
+
+    const dbSecret = new secretsmanager.Secret(this, "Secret")
+    const dbPassword = dbSecret.secretValue
+    const role = new iam.Role(this, "FargateRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonECSTaskExecutionRolePolicy")],
+    })
+    dbSecret.grantRead(role)
+
+    /* 
+
+    RDS DATABASE SETUP
+
+    */
+
+    const dbName = process.env.DB_NAME ? process.env.DB_NAME : "postgresDB"
+    const dbUsername = process.env.DB_USERNAME ? process.env.DB_USERNAME : "dbUser"
+    const dbPort = "5432"
+
+    const dbInstance = new rds.DatabaseInstance(this, "Instance", {
+      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_10}),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE,
+      },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromPassword(dbUsername, dbPassword),
+      databaseName: dbName,
+    })
+
+    const dbHost = dbInstance.dbInstanceEndpointAddress
+
+    /* 
+
+        Fargate Service
+
+    */
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc,
+    })
+
+    const securityGroupFargate = new ec2.SecurityGroup(this, `SG-Fargate`, {
+      vpc,
+    })
+
+    securityGroupFargate.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80))
+
+    const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      cpu: 512,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset("../server"),
+        taskRole: role,
+        secrets: {
+          DB_PASSWORD: ecs.Secret.fromSecretsManager(dbSecret),
+        },
+        environment: {
+          NODE_ENV: "production",
+          DB_HOST: dbHost,
+          DB_PORT: dbPort,
+          DB_NAME: dbName,
+          DB_USER: dbUsername,
+        },
+      },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true,
+      securityGroups: [securityGroupFargate],
+    })
+
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/health",
+    })
+
+    //loadBalancedFargateService.loadBalancer.loadBalancerDnsName
+
+    /* 
+
+        Bastion Host
+
+    */
 
     const securityGroupBastion = new ec2.SecurityGroup(this, "bastion-security", {
       vpc,
@@ -27,60 +115,8 @@ export class InfrastructureStack extends cdk.Stack {
       securityGroup: securityGroupBastion,
     })
 
-    /* 
-
-        Fargate Service
-
-    */
-    const cluster = new ecs.Cluster(this, "Cluster", {
-      vpc,
-    })
-
-    const securityGroupFargate = new ec2.SecurityGroup(this, `security-group`, {
-      vpc,
-    })
-
-    securityGroupFargate.addIngressRule(ec2.Peer.ipv4("0.0.0.0/0"), ec2.Port.tcp(80))
-
-    const loadBalancedFargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster,
-      cpu: 512,
-      taskImageOptions: {image: ecs.ContainerImage.fromAsset("../server")},
-      memoryLimitMiB: 2048,
-      publicLoadBalancer: true,
-      securityGroups: [securityGroupFargate],
-    })
-
-    loadBalancedFargateService.targetGroup.configureHealthCheck({
-      path: "/health",
-    })
-
-    /* 
-
-    RDS DATABASE SETUP
-
-    */
-
-    const db_name = "postgresDB"
-    const db_username = "DBadmin"
-
-    const dbInstance = new rds.DatabaseInstance(this, "Instance", {
-      engine: rds.DatabaseInstanceEngine.postgres({version: rds.PostgresEngineVersion.VER_10}),
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE,
-      },
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      credentials: rds.Credentials.fromGeneratedSecret(db_username),
-      databaseName: db_name,
-    })
-
+    //allow connections to db from fargate and bastion host
     dbInstance.connections.allowFrom(securityGroupFargate, ec2.Port.tcp(5432))
     dbInstance.connections.allowFrom(securityGroupBastion, ec2.Port.tcp(5432))
-
-    //const dbHost = dbInstance.dbInstanceEndpointAddress
-    //var PostgresPassword = new secretsmanager.Secret(this, "PersonalSecOpsMySqlPassword",{
-    //generateSecretString }
-    //);
   }
 }
