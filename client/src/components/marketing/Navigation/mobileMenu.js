97434c6ce728a222ee2ad5d802ece03b0791755c
ioannaxen <ioannatxenaki@gmail.com>
2020-11-17 21:44:16 +0000

    MobileMenu styling

--- client/src/components/marketing/Navigation/mobileMenu.js
+++ client/src/components/marketing/Navigation/mobileMenu.js
@@ -1,6 +1,89 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Link from 'next/link';
-import { colors } from '../../../styles/theme';
+import { colors, breakpoints } from '../../../styles/theme';
+
+const Wrapper2 = styled.div`
+  border-radius: 0.5rem;
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+`;
+
+const Wrapper3 = styled.div`
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05);
+  background-color: ${colors.white};
+  border-color: ${colors.gray50};
+`;
+
+const Wrapper4 = styled.div`
+  padding: 1.25rem 1.25rem 1.5rem;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
+}
+`;
+
+const LogoImage = styled.img`
+  height: 2rem;
+  width: auto;
+`;
+
+const CloseButtonWrapper = styled.div`
+  margin-right: -0.5rem;
+`;
+
+const CloseButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+  border-radius: 0.375rem;
+  color: ${colors.gray400};
+  &:hover {
+    color: ${colors.gray500};
+    background-color: ${colors.gray100};
+  }
+  &:focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+    color: ${colors.gray500};
+    background-color: ${colors.gray100};
+  }
+  transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+    transform;
+  transition-duration: 150ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+`;
+
+const CloseImage = styled.img`
+  height: 1.5rem;
+  width: 1.5rem;
+`;
+
+const Nav = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  grid-gap: 1.75rem;
+  gap: 1.75rem;
+`;
+
+const Item = styled.div`
+  margin: -0.75rem;
+  padding: 0.75rem;
+  display: flex;
+  align-items: center;
+  border-radius: 0.5rem;
+  &:hover {
+    background-color@ ${colors.gray50};
+  }
+  transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow, transform;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+`;
 
 const MenuImageWrapper = styled.div`
   flex-shrink: 0;
@@ -21,114 +104,170 @@ const MenuImg = styled.img`
 `;
 
 const Title = styled.div`
+  padding-left: 1rem;
   font-size: 1rem;
   line-height: 1.5rem;
   font-weight: 500;
   color: ${colors.gray900};
 `;
 
+const SmallHeader = styled.h2`
+  font-size: 1.125rem;
+`;
+
+const ButtonWrapper1 = styled.div`
+  margin-top: 1.5rem;
+  padding: 0 1.25rem 1.5rem;
+`;
+
+const ButtonWrapper2 = styled.div`
+  margin-top: 1.5rem;
+`;
+
+const Button = styled.div`
+  cursor: pointer;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  border-width: 1px;
+  border-color: transparent;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 500;
+  border-radius: 0.375rem;
+  color: ${colors.white};
+  background-color: ${colors.indigo600};
+  &:hover {
+    background-color: ${colors.indigo500};
+  }
+  &:focus {
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+    border-color: ${colors.indigo700};
+    box-shadow: 0 0 0 3px rgba(180, 198, 252, 0.45);
+  }
+  &:active {
+    background-color: ${colors.indigo700};
+  }
+  transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+    transform;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+`;
+
+const menuUp = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.95);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }`;
+
+const Wrapper1 = styled.div`
+  animation: ${menuUp} 0.3s ease-out forwards;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  padding: 0.5rem;
+  ransition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+    transform;
+  ransition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+    transform;
+  transform-origin: top right;
+  @media (min-width: ${breakpoints.medium}) {
+    display: none;
+  }
+`;
+
 const MobileMenu = ({ mobileMenuHandler }) => (
-  <div className='MenuScale absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden'>
-    <div className='rounded-lg shadow-lg'>
-      <div className='rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50'>
-        <div className='pt-5 pb-6 px-5 space-y-6'>
-          <div className='flex items-center justify-between py-4'>
-            <img className='h-8 w-auto' src='/logo/small_logo.svg' alt='Workflow' />
-            <div className='-mr-2'>
-              <button
-                onClick={mobileMenuHandler}
-                type='button'
-                className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out'
-              >
-                <img className='h-6 w-6' src='/icons/close.svg' alt='menu icon' />
-              </button>
-            </div>
-          </div>
+  <Wrapper1>
+    <Wrapper2>
+      <Wrapper3>
+        <Wrapper4>
+          <Header>
+            <LogoImage src='/logo/small_logo.svg' alt='Workflow' />
+            <CloseButtonWrapper>
+              <CloseButton onClick={mobileMenuHandler} type='button'>
+                <CloseImage src='/icons/close.svg' alt='menu icon' />
+              </CloseButton>
+            </CloseButtonWrapper>
+          </Header>
           <div>
-            <nav className='grid grid-cols-1 gap-7'>
+            <Nav>
               <Link href='/pricing'>
-                <div
-                  href='#'
-                  className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                >
+                <Item>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: view-grid -->*/}
                     <MenuImg src='/icons/view-grid.svg' alt='click' />
                   </MenuImageWrapper>
                   <Title>Pricing</Title>
-                </div>
+                </Item>
               </Link>
               <Link href='/app'>
-                <div className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'>
+                <Item>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: view-grid -->*/}
                     <MenuImg src='/icons/view-grid.svg' alt='click' />
                   </MenuImageWrapper>
                   <Title>App</Title>
-                </div>
+                </Item>
               </Link>
               <hr />
-              <h2 className='text-lg'>Solutions: </h2>
+              <SmallHeader>Solutions: </SmallHeader>
               <Link href='#'>
-                <div className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'>
+                <Item>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: chart-bar -->*/}
                     <MenuImg src='/icons/chart-bar.svg' alt='chart bar' />
                   </MenuImageWrapper>
                   <Title>Analytics</Title>
-                </div>
+                </Item>
               </Link>
               <Link href='#'>
-                <div
-                  href='#'
-                  className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                >
+                <Item href='#'>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: cursor-click -->*/}
                     <MenuImg src='/icons/cursor-click.svg' alt='click' />
                   </MenuImageWrapper>
                   <Title>Engagement</Title>
-                </div>
+                </Item>
               </Link>
               <Link href='#'>
-                <div
-                  href='#'
-                  className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                >
+                <Item href='#'>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: shield-check -->*/}
                     <MenuImg src='/icons/shield-check.svg' alt='click' />
                   </MenuImageWrapper>
                   <Title>Security</Title>
-                </div>
+                </Item>
               </Link>
               <Link href='#'>
-                <div
-                  href='#'
-                  className='-m-3 p-3 flex items-center space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150'
-                >
+                <Item href='#'>
                   <MenuImageWrapper>
                     {/*<!-- Heroicon name: view-grid -->*/}
                     <MenuImg src='/icons/view-grid.svg' alt='click' />
                   </MenuImageWrapper>
                   <Title>Integrations</Title>
-                </div>
+                </Item>
               </Link>
-            </nav>
+            </Nav>
           </div>
-        </div>
-        <div className='py-6 px-5 space-y-6'>
-          <div className='space-y-6'>
+        </Wrapper4>
+        <ButtonWrapper1>
+          <ButtonWrapper2>
             <Link href='/login'>
-              <div className='w-full flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150'>
-                Sign up
-              </div>
+              <Button>Sign up</Button>
             </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+          </ButtonWrapper2>
+        </ButtonWrapper1>
+      </Wrapper3>
+    </Wrapper2>
+  </Wrapper1>
 );
 
 export default MobileMenu;
