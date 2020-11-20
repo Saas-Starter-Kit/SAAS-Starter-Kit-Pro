9f704bcb64a5e62d15d1b037902f567d486e204b
iqbal125 <iqbal125@yahoo.com>
2020-11-19 17:09:41 -0800

    remove integrated cicd

--- infra/bin/infra.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { InfraStack } from '../lib/infra-stack';
-
-const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
