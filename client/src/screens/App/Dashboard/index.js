e670de109b3fc4a139a9ce68ed62ef755ab9d35c
ioannaxen <ioannatxenaki@gmail.com>
2020-11-21 18:01:31 +0000

    Dashboard styling

--- client/src/screens/App/Dashboard/index.js
+++ client/src/screens/App/Dashboard/index.js
@@ -1,14 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
 import Stats from './stats';
 import ActivityList from './activityList';
+import { colors } from '../../../styles/theme';
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h1 className='text-2xl font-semibold text-gray-900'>Dashboard</h1>
-      <Stats />
-      <ActivityList />
-    </div>
-  );
-};
+const Title = styled.h1`
+  font-weight: 600;
+  color: ${colors.gray900};
+  font-size: 1.5rem;
+`;
+
+const Dashboard = () => (
+  <React.Fragment>
+    <Title>Dashboard</Title>
+    <Stats />
+    <ActivityList />
+  </React.Fragment>
+);
 
 export default Dashboard;
