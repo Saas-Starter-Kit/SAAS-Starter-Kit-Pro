b3e95f0b2c930170bd6ee0db5ff98141bac1839e
ioannaxen <ioannatxenaki@gmail.com>
2020-11-21 13:25:16 +0000

    SidebarDesktop styling

--- client/src/components/app/Navigation/sidebarDesktop.js
+++ client/src/components/app/Navigation/sidebarDesktop.js
@@ -1,8 +1,24 @@
 import styled, { keyframes } from 'styled-components';
-import { Link } from '@reach/router';
 import { BiArrowFromRight } from 'react-icons/bi';
 import Avatar from '../Avatar/avatarSidebarFull';
-import { colors } from '../../../styles/theme';
+import { colors, breakpoints } from '../../../styles/theme';
+import SidebarItem from './sidebarItem';
+import Home from '../../svgs/home';
+import Persons from '../../svgs/persons';
+import Folder from '../../svgs/folder';
+
+const Wrapper1 = styled.div`
+  display: none;
+  @media (min-width: ${breakpoints.medium}) {
+    display: flex;
+    flex-shrink: 0;
+  }
+`;
+
+const Wrapper2 = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const growSideBar = keyframes`
 from {
@@ -12,7 +28,7 @@ to {
   width: 14rem;
 }`;
 
-const Sidebar = styled.div`
+const SidebarWrapper1 = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1 1 0%;
@@ -20,6 +36,33 @@ const Sidebar = styled.div`
   width: 14rem;
   overflow: hidden;
   animation: ${growSideBar} 0.3s ease-out forwards;
+`;
+
+const SidebarWrapper2 = styled.div`
+  flex: 1 1 0%;
+  display: flex;
+  flex-direction: column;
+  padding-top: 1.25rem;
+  padding-bottom: 1rem;
+  overflow-y: auto;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  padding-left: 1.5rem;
+`;
+
+const Logo = styled.img`
+  height: 2rem;
+  width: auto;
+`;
+
+const Nav = styled.nav`
+  margin-top: 2rem;
+  flex: 1 1 0%;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  background-color: ${colors.indigo800};
 `;
 
 const fadeInRight = keyframes`
@@ -45,86 +88,27 @@ const StyledArrow = styled(BiArrowFromRight)`
   animation: ${fadeInRight} 0.7s ease-in forwards;
 `;
 
-const SidebarDesktop = ({ sidebarHandler }) => {
-  return (
-    <div className='hidden md:flex md:flex-shrink-0'>
-      <div className='flex flex-col'>
-        <Sidebar>
-          <div className='flex-1 flex flex-col pt-5 pb-4 overflow-y-auto'>
-            <div className='flex pl-6'>
-              <img className='h-8 w-auto' src='/logo/large_logo.svg' alt='Workflow' />
-            </div>
-
-            <Avatar />
-            <nav className='mt-8 flex-1 px-2 bg-indigo-800 space-y-1'>
-              <Link to='/app'>
-                <div className='group flex items-center px-2 py-4 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'>
-                  <svg
-                    className='mr-3 h-6 w-6 text-indigo-400 group-focus:text-indigo-300 transition ease-in-out duration-150'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
-                    />
-                  </svg>
-                  Dashboard
-                </div>
-              </Link>
-
-              <Link to='/app/readupdate'>
-                <div className='group flex items-center px-2 py-4 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'>
-                  <svg
-                    className='mr-3 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'
-                    />
-                  </svg>
-                  Read Update
-                </div>
-              </Link>
-
-              <Link to='/app/create'>
-                <div className='group flex items-center px-2 py-4 text-sm leading-5 font-medium text-indigo-300 rounded-md hover:text-white hover:bg-indigo-700 focus:outline-none focus:text-white focus:bg-indigo-700 transition ease-in-out duration-150'>
-                  <svg
-                    className='mr-3 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    stroke='currentColor'
-                  >
-                    <path
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                      stroke-width='2'
-                      d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
-                    />
-                  </svg>
-                  Create
-                </div>
-              </Link>
-            </nav>
-          </div>
-        </Sidebar>
-        <div onClick={sidebarHandler}>
-          <StyledArrow />
-        </div>
+const SidebarDesktop = ({ sidebarHandler }) => (
+  <Wrapper1>
+    <Wrapper2>
+      <SidebarWrapper1>
+        <SidebarWrapper2>
+          <LogoWrapper>
+            <Logo src='/logo/large_logo.svg' alt='Workflow' />
+          </LogoWrapper>
+          <Avatar />
+          <Nav>
+            <SidebarItem link='/app' title='Dashboard' svg={<Home />} />
+            <SidebarItem link='/app/readupdate' title='Read Update' svg={<Persons />} />
+            <SidebarItem link='/app/create' title='Create' svg={<Folder />} />
+          </Nav>
+        </SidebarWrapper2>
+      </SidebarWrapper1>
+      <div onClick={sidebarHandler}>
+        <StyledArrow />
       </div>
-    </div>
-  );
-};
+    </Wrapper2>
+  </Wrapper1>
+);
 
 export default SidebarDesktop;
--- client/src/components/svgs/folder.js
+++ client/src/components/svgs/folder.js
@@ -14,15 +14,17 @@ const Svg = styled.svg`
   }
 `;
 
-const Folder = () => (
-  <Svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-    <path
-      stroke-linecap='round'
-      stroke-linejoin='round'
-      stroke-width='2'
-      d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
-    />
-  </Svg>
+const Folder = ({ className }) => (
+  <div className={className}>
+    <Svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+      <path
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+        d='M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z'
+      />
+    </Svg>
+  </div>
 );
 
 export default Folder;
