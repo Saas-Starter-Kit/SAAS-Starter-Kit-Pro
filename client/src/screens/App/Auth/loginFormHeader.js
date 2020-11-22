65b6cfd3cf613b7ead26ae3883016376a125b596
ioannaxen <ioannatxenaki@gmail.com>
2020-11-22 12:59:04 +0000

    Auth Screens styling

--- client/src/screens/App/Auth/loginFormHeader.js
+++ client/src/screens/App/Auth/loginFormHeader.js
@@ -1,16 +1,36 @@
-const LoginFormHeader = () => {
-  return (
-    <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-      <img
-        className='mx-auto h-12 w-auto'
-        src='https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg'
-        alt='Workflow'
-      />
-      <h2 className='mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900'>
-        Sign-In or Sign-Up for an account
-      </h2>
-    </div>
-  );
-};
+import styled from 'styled-components';
+import { colors, breakpoints } from '../../../styles/theme';
+
+const Wrapper = styled.div`
+  @media (min-width: ${breakpoints.small}) {
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    max-width: 28rem;
+  }
+`;
+
+const Image = styled.img`
+  margin-left: auto;
+  margin-right: auto;
+  height: 3rem;
+  width: auto;
+`;
+
+const Title = styled.h2`
+  margin-top: 1.5rem;
+  color: ${colors.gray900};
+  text-align: center;
+  font-weight: 800;
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+`;
+
+const LoginFormHeader = () => (
+  <Wrapper>
+    <Image src='https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg' alt='Workflow' />
+    <Title>Sign-In or Sign-Up for an account</Title>
+  </Wrapper>
+);
 
 export default LoginFormHeader;
