d4d5633fa709c335c210a2d0df5a9a778a6870ff
iqbal125 <iqbal125@yahoo.com>
2020-12-02 18:01:10 -0800

    usecontext update

--- client/src/components/app/Routes/index.js
+++ client/src/components/app/Routes/index.js
@@ -1,3 +1,4 @@
+import { useContext, useEffect } from 'react';
 import AppLayout from '../AppLayout';
 import { Router } from '@reach/router';
 import Dashboard from '../../../screens/App/Dashboard';
@@ -5,9 +6,34 @@ import Settings from '../../../screens/App/Settings';
 import Create from '../../../screens/App/Create';
 import ReadUpdate from '../../../screens/App/ReadUpdate';
 import { useRouter } from 'next/router';
+import AuthContext from '../../../utils/authContext';
 
 const Routes = () => {
+  const { LogIn, LogOut } = useContext(AuthContext);
+
   const router = useRouter();
+
+  const silentAuth = () => {
+    let user, expiresAt;
+
+    user = JSON.parse(localStorage.getItem('user'));
+    expiresAt = JSON.parse(localStorage.getItem('expiresIn'));
+
+    console.log(user);
+    if (user && new Date().getTime() < expiresAt) {
+      LogIn();
+      console.log('fffff');
+    } else if (!user || new Date().getTime() > expiresAt) {
+      LogOut();
+      console.log('rrrrr');
+    }
+  };
+
+  useEffect(() => {
+    if (!typeof window === 'undefined') {
+      setTimeout(() => silentAuth(), 300);
+    }
+  }, []);
 
   //check token expires time on private routes
   const isTokenValid = () => {
@@ -27,7 +53,8 @@ const Routes = () => {
   return (
     <AppLayout>
       <Router>
-        {/*<PrivateRoute path="/app" component={Dashboard}/>*/}
+        {/*<PrivateRoute path='/app' component={Dashboard} />*/}
+
         <Dashboard path='/app' />
         <Settings path='/app/settings' />
         <Create path='/app/create' />
