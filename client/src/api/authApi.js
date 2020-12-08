995ba86017c0167e3a20aece87d8931ff5348676
iqbal125 <iqbal125@yahoo.com>
2020-12-07 19:21:41 -0800

    change api base url

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,13 +1,15 @@
 //API requests related to authentication
-import axios from 'axios';
+import axios from "axios"
+
+const axiosBase = process.env.GATSBY_SERVER_URL
 
 //Send Firebase auth token to authenticate against our own server
 export const sendtokenToServer = async (token, username) => {
-  const data = { token, username };
+  const data = { token, username }
 
-  const result = await axios.post(`${process.env.React_APP_SERVER_URL}/auth/sendtoken`, data);
+  const result = await axios.post(`${axiosBase}/auth/sendtoken`, data)
 
-  return result;
-};
+  return result
+}
 
-export const updateUserNameApi = async () => {};
+export const updateUserNameApi = async () => {}
