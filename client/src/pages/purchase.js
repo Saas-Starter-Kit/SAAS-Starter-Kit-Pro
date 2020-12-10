91c61d2b76dc7ae42fc05a6ebe8b5b984c559472
iqbal125 <iqbal125@yahoo.com>
2020-12-10 12:10:57 -0800

    add purchase page

---
+++ client/src/pages/purchase.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Purchase from "../screens/Admin/Purchase"
+import Layout from "../components/Marketing/Layout"
+
+export default function LoginPage() {
+  return (
+    <Layout>
+      <Purchase />
+    </Layout>
+  )
+}
---
+++ client/src/screens/Admin/Purchase/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Purchase = () => {
+  return <div>Purchase</div>
+}
+
+export default Purchase
