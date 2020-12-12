8b876f0c433e42b2f5c44faf21dd8a0674dd1674
iqbal125 <iqbal125@yahoo.com>
2020-12-12 12:37:33 -0800

    add subscription route  server

--- server/src/API/stripe.js
+++ server/src/API/stripe.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import { CreateCustomer, CreateSetupIntent } from '../Services/stripe.js';
+import { CreateCustomer, CreateSetupIntent, CreateSubscription } from '../Services/stripe.js';
 
 router.post('/customer', CreateCustomer);
 
 router.post('/wallet', CreateSetupIntent);
+
+router.post('/subscription', CreateSubscription);
 
 export default router;
--- server/src/Services/stripe.js
+++ server/src/Services/stripe.js
@@ -10,7 +10,29 @@ export const CreateCustomer = async (req, res) => {
 export const CreateSetupIntent = async (req, res) => {
   const setupIntent = await stripe.setupIntents.create({
     customer: 'cus_IY1vr2IUrT3e6Z'
-  })
-  
+  });
+
   res.send(setupIntent);
+};
+
+export const CreateSubscription = async (req, res) => {
+  let customer = 'cus_IY1vr2IUrT3e6Z';
+  let payment_method = 'pm_1HwxtwAtqjBKUOx98KEzuuWh';
+  let plan = 'prod_IWXuNM17jweetX';
+
+  // Attach the  payment method to the customer
+  await stripe.paymentMethods.attach(payment_method, { customer });
+
+  // Set it as the default payment method
+  await stripe.customers.update(customer, {
+    invoice_settings: { default_payment_method: payment_method }
+  });
+
+  const subscription = await stripe.subscriptions.create({
+    customer: customer,
+    items: [{ plan }],
+    expand: ['latest_invoice.payment_intent']
+  });
+
+  res.send(subscription);
 };
