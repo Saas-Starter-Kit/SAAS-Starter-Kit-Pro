2656c0539a7f727eb0a5ec9db3b15cfa32a3064b
iqbal125 <iqbal125@yahoo.com>
2020-12-13 17:49:09 -0800

    style purchase page

--- client/src/screens/Admin/Purchase/checkoutForm.js
+++ client/src/screens/Admin/Purchase/checkoutForm.js
@@ -2,6 +2,87 @@ import React, { useState, useEffect, useContext } from "react"
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
 import axios from "axios"
 import AuthContext from "../../../utils/authContext"
+import { colors, breakpoints, fieldStyles } from "../../../styles/theme"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  background-color: ${colors.gray50};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: -2rem;
+
+  @media (min-width: ${breakpoints.small}) {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+  @media (min-width: ${breakpoints.large}) {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+`
+
+const ButtonWrapper = styled.div`
+  padding-top: 2rem;
+  padding-bottom: 1rem;
+  background-color: ${colors.white};
+  text-align: left;
+`
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  font-weight: 500;
+  width: 100%;
+  color: ${colors.white};
+  background-color: ${colors.indigo600};
+  border: 1px solid transparent;
+  border-radius: 0.375rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  cursor: pointer;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  &:hover {
+    background-color: ${colors.indigo500};
+  }
+  &:focus {
+    box-shadow: 0 0 0 3px rgba(164, 202, 254, 0.45);
+    outline: 2px solid transparent;
+    outline-offset: 2px;
+  }
+  &:active {
+    background-color: ${colors.indigo600};
+  }
+  transition-property: background-color, border-color, color, fill, stroke,
+    opacity, box-shadow, transform;
+  transition-duration: 150ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+`
+
+const CardWrapper = styled.div`
+  padding-left: 2rem;
+  padding-right: 2rem;
+
+  @media (min-width: ${breakpoints.small}) {
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    width: 100%;
+    max-width: 28rem;
+  }
+`
+
+const Card = styled.div`
+  background-color: ${colors.white};
+  padding: 2rem 1rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  @media (min-width: ${breakpoints.small}) {
+    border-radius: 0.5rem;
+    padding-left: 2.5rem;
+    padding-right: 2.5rem;
+  }
+`
 
 const CheckoutForm = () => {
   const { authState } = useContext(AuthContext)
@@ -45,17 +126,20 @@ const CheckoutForm = () => {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <CardElement />
-        <button type="submit" disabled={!stripe && !setupIntentState}>
-          Pay
-        </button>
-      </form>
-      <button onClick={() => console.log(authState, setupIntentState)}>
-        DDDDDD
-      </button>
-    </>
+    <Wrapper>
+      <CardWrapper>
+        <Card>
+          <form onSubmit={handleSubmit}>
+            <CardElement />
+            <ButtonWrapper>
+              <Button type="submit" disabled={!stripe && !setupIntentState}>
+                Pay
+              </Button>
+            </ButtonWrapper>
+          </form>
+        </Card>
+      </CardWrapper>
+    </Wrapper>
   )
 }
 
