f7473972c10acadc15056ac7079d29e48982f964
iqbal125 <iqbal125@yahoo.com>
2020-12-14 17:50:36 -0800

    add google analytics

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,18 +1,25 @@
-require("dotenv").config()
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
     defaultTitle: `Gatsby SAAS Starter`,
     defaultDescription: `A starter for a minimal SAAS app`,
     siteUrl: `https://example.com`,
-    defaultImage: "./src/images/favicon.ico",
+    defaultImage: './src/images/favicon.ico'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/app/*`] },
+      options: { prefixes: [`/app/*`] }
     },
-  ],
-}
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_ID,
+        head: true
+      }
+    }
+  ]
+};
