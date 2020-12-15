3d83305a03fb70bb302d8969b27b1b67b514660c
iqbal125 <iqbal125@yahoo.com>
2020-12-14 17:35:55 -0800

    add 404 page

---
+++ client/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Marketing/Layout';
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <h2>Page Not Found</h2>
+    </Layout>
+  );
+}
