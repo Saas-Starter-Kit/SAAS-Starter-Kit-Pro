17f19110ee95c2c312cc733f19b6217b4892007f
iqbal125 <iqbal125@yahoo.com>
2020-12-20 17:06:27 -0800

    extract check user function

--- server/src/Services/authentication.js
+++ server/src/Services/authentication.js
@@ -4,79 +4,88 @@ import firebase from 'firebase-admin';
 
 const admin = firebase.initializeApp();
 
-export const SignUp = (req, res) => {
+const CheckUserExists = async (email, next) => {
+  //check if email exists
+  let text = `SELECT * FROM users
+              WHERE email=$1`;
+
+  let values = [email];
+
+  let queryResult = await db.query(text, values).catch((err) => {
+    console.log(err);
+    return { type: 'error', message: 'Database query failed' };
+  });
+
+  if (!queryResult) {
+    return { type: 'error', message: 'Database query failed' };
+  } else if (queryResult.rows.length != 0) {
+    return { type: 'error', message: 'User Already Exists' };
+  } else if (queryResult.rows.length === 0) {
+    return { type: 'success', message: 'User Not Found' };
+  } else {
+    return { type: 'error', message: 'Database query failed' };
+  }
+};
+
+export const SignUp = async (req, res, next) => {
   let token = req.body.token;
   let username = req.body.username;
 
-  const saveUsertoDB = (email, username) => {
-    /* Save user to our own db and get unique key from db */
+  let email = req.body.email;
+  console.log(email);
 
-    //check if email exists
-    let query1 = `SELECT * FROM users
-                WHERE email=$1`;
+  let userExists = await CheckUserExists(email, next).catch((err) => {
+    console.log(err);
+    res.status(500).send('Database query failed');
+    throw new Error('Database query Failed');
+  });
 
-    //if email not found insert into database
-    let query2 = `INSERT INTO users (username, email)
-                VALUES($1, $2)
-                RETURNING id`;
+  if (!userExists) {
+    res.status(500).send('Database query failed');
+  } else if (userExists.type === 'error') {
+    res.status(500).send(userExists.message);
+  } else if (userExists.type === 'success') {
+    //res.status(500).send(userExists.message);
+  }
 
-    let values1 = [email];
-
-    let values2 = [username, email];
-
-    //signup user, called inside callback1
-    let callback2 = (q_err, q_res) => {
-      if (q_err) {
-        console.log(q_err);
-        res.status(500).send(q_err);
-      }
-      //send back user id after signup
-      if (q_res.rows[0]) {
-        let id = q_res.rows[0].id;
-
-        console.log(q_res.rows);
-        //jwt token login after signup
-        res.send({ token: setToken(id) });
-      }
-    };
-
-    //Check if user exists callback
-    let callback1 = (q_err, q_res) => {
-      if (q_err) {
-        console.log(q_err);
-        res.status(500).send(q_err);
-      }
-      if (q_res.rows.length != 0) {
-        let response = {
-          type: 'error',
-          message: 'User Already Exists'
-        };
-
-        res.send(response);
-      }
-      if (q_res.rows.length === 0) {
-        //if email not found, create user in db
-        db.query(query2, values2, callback2);
-      }
-    };
-
-    //check if user exists
-    db.query(query1, values1, callback1);
-  };
-
-  admin
-    .auth()
-    .verifyIdToken(token)
-    .then((decodedToken) => {
-      let name = username ? username : decodedToken.email;
-      let email = decodedToken.email;
-
-      saveUsertoDB(email, name);
-    })
-    .catch((error) => {
-      res.send('error signing up');
-      console.log(error);
-    });
+  console.log(userExists);
+  //let token = req.body.token;
+  //let username = req.body.username;
+  //const saveUsertoDB = (email, username) => {
+  /* Save user to our own db and get unique key from db */
+  //  //if email not found insert into database
+  //  let query2 = `INSERT INTO users (username, email)
+  //              VALUES($1, $2)
+  //              RETURNING id`;
+  //  let values2 = [username, email];
+  //  //signup user, called inside callback1
+  //  let callback2 = (q_err, q_res) => {
+  //    if (q_err) {
+  //      console.log(q_err);
+  //      res.status(500).send(q_err);
+  //    }
+  //    //send back user id after signup
+  //    if (q_res.rows[0]) {
+  //      let id = q_res.rows[0].id;
+  //      console.log(q_res.rows);
+  //      //jwt token login after signup
+  //      res.send({ token: setToken(id) });
+  //    }
+  //  };
+  //  //check if user exists
+  //};
+  //admin
+  //  .auth()
+  //  .verifyIdToken(token)
+  //  .then((decodedToken) => {
+  //    let name = username ? username : decodedToken.email;
+  //    let email = decodedToken.email;
+  //    saveUsertoDB(email, name);
+  //  })
+  //  .catch((error) => {
+  //    res.send('error signing up');
+  //    console.log(error);
+  //  });
 };
 
 export const Login = (req, res) => {
--- server/src/Services/health.js
+++ server/src/Services/health.js
@@ -2,6 +2,10 @@ export const healthCheck = (req, res) => {
   res.status(200).send('All ok');
 };
 
+export const failHealthCheck = (req, res) => {
+  res.status(404).send('Failed Health Check');
+};
+
 export const privateRoute = (req, res) => {
   res.send('Accessed Private Endpoint');
 };
