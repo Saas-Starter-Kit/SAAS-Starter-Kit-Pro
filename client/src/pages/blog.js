d173089f54947a8d362a2fde0cf552ca5161cb79
iqbal125 <iqbal125@yahoo.com>
2020-12-30 14:03:27 -0800

    add blog and docs screens

---
+++ client/src/pages/blog.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Layout from '../components/Marketing/Layout';
+
+export default function BlogPage() {
+  return <div />;
+}
---
+++ client/src/screens/Marketing/Docs/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Docs = () => {
+  return <div>Docs</div>;
+};
+
+export default Docs;
