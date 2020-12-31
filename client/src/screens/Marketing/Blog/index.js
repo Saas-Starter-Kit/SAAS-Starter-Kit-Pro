df5ee3de2ffd84d2318bc58260f4c159ac94094c
iqbal125 <iqbal125@yahoo.com>
2020-12-30 18:39:45 -0800

    add blog

--- client/src/screens/Marketing/Blog/index.js
+++ client/src/screens/Marketing/Blog/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const data = useStaticQuery(staticQuery);
+  const { allPrismicPost } = data;
+  const { edges } = allPrismicPost;
+
+  const blogLinks = edges;
+
+  return (
+    <Wrapper>
+      <div>
+        <h3>Featured:</h3>
+        {/*hardcode*/}
+      </div>
+      {blogLinks.map((blogLink) => (
+        <div>
+          <Link to={`/blog/${blogLink.node.uid}`}>{blogLink.node.data.title.text}</Link>
+          <p>{blogLink.node.data.date}</p>
+        </div>
+      ))}
+    </Wrapper>
+  );
 };
 
 export default Blog;
+
+const staticQuery = graphql`
+  query BlogAll {
+    allPrismicPost {
+      edges {
+        node {
+          uid
+          data {
+            title {
+              text
+            }
+            date
+          }
+        }
+      }
+    }
+  }
+`;
