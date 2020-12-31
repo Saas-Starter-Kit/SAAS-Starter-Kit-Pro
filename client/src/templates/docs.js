fa0d6685c849f0129dfc0c145cf8994fc462eed0
iqbal125 <iqbal125@yahoo.com>
2020-12-31 13:16:58 -0800

    test quote

--- client/src/templates/docs.js
+++ client/src/templates/docs.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import SliceZone from './sliceZone';
 import DocsSidebar from '../screens/Marketing/Docs/docsSidebar';
 
+//test quote
 const Wrapper = styled.div`
   display: flex;
 `;
