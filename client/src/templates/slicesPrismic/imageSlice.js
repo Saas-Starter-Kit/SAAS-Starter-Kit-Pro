b680af2879142ce7c30f777802b90b3e358c316e
iqbal125 <iqbal125@yahoo.com>
2021-01-04 18:51:10 -0800

    fix image width

--- client/src/templates/slicesPrismic/imageSlice.js
+++ client/src/templates/slicesPrismic/imageSlice.js
@@ -2,6 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 //import Img from 'gatsby-image';
 
-const Image = ({ slice }) => <div>{console.log(slice)}</div>;
+const Image = ({ slice }) => {
+  const desktop = slice.primary.image.thumbnails.desktop;
+  const mobile = slice.primary.image.thumbnails.mobile;
+
+  return (
+    <picture>
+      <source width="100%" srcSet={mobile.url} alt={mobile.alt} media="(max-width: 678px)" />
+      <img width="100%" src={desktop.url} alt={desktop.alt} />
+    </picture>
+  );
+};
 
 export default Image;
