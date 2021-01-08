1fe599a768700d1b4e4d8959b9654ea2618605bd
iqbal125 <iqbal125@yahoo.com>
2021-01-07 18:08:42 -0800

    extract out purchase route

--- client/src/pages/purchase.js
+++ client/src/pages/purchase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Purchase from '../screens/App/Purchase';
+import Purchase from '../screens/User/Purchase';
 
 export default function LoginPage() {
   return <Purchase />;
