8735d0bc571d5d5c8e44b08944d6c682af0a1886
iqbal125 <iqbal125@yahoo.com>
2021-02-01 13:50:17 -0800

    add permissions middleware

--- server/src/API/utils.js
+++ server/src/API/utils.js
@@ -4,23 +4,28 @@ const router = express.Router();
 import { requireAuth } from '../Config/passport.js';
 import { _healthCheck, privateRoute, failHealthCheck } from '../Services/utils/health.js';
 import { sendEmail } from '../Services/utils/emailTest.js';
-import { Permissions } from '../Services/utils/permissionsTest.js';
 import { asyncHandler } from '../Utils/asyncErrorHandler.js';
+import { requirePermissions } from '../Config/permissions.js';
 
-//Permission Based Route
-router.post('/permissions', asyncHandler(Permissions));
-router.get('/permissions', asyncHandler(Permissions));
-
-//Health Routes
+//intentionally fail health check for development/testing
 router.get('/fail-health', asyncHandler(failHealthCheck));
 
+//test email templates without sending them to real users
 router.get('/send-email', asyncHandler(sendEmail));
 
-//Example of authenticated route
-router.get('/private', requireAuth, asyncHandler(privateRoute));
+//Example of authenticated route, use for development/testing
+router.get('/private/auth', requireAuth, asyncHandler(privateRoute));
+router.post('/private/auth', requireAuth, asyncHandler(privateRoute));
+
+//Example of permissions route, use for development/testing
+router.get('/private/permissions', requirePermissions, asyncHandler(privateRoute));
+router.post('/private/permissions', requirePermissions, asyncHandler(privateRoute));
+
+//Example of authenticated and permissions route
+router.get('/private/authpermissions', requireAuth, requirePermissions, asyncHandler(privateRoute));
 
 /* 
-    DO NOT MODIFY, '/health' is used by AWS Fargate 
+    DO NOT MODIFY is using AWS, '/health' is used by AWS Fargate 
     to run a health check and determine when to 
     launch a new container. 
 */
--- server/src/Config/permissions.js
+++ server/src/Config/permissions.js
@@ -33,3 +33,19 @@ export const permissionsMiddleware = (req, _, next) => {
   req.ability = defineAbilityFor(role);
   next();
 };
+
+export const requirePermissions = (req, res, next) => {
+  let userAction = req.body.userAction || req.query.userAction;
+  let subject = req.body.subject || req.query.subject;
+
+  if (!req.ability.can(userAction, subject)) {
+    let error = {
+      type: '403 Forbidden',
+      message: 'User does not have access to this resource'
+    };
+
+    res.status(403).send(error);
+  } else {
+    next();
+  }
+};
