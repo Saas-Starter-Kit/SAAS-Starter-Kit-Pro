7ba45eb2aa3f0044e1e9b4c009ff93b9ac8dc16d
iqbal125 <iqbal125@yahoo.com>
2021-02-06 19:28:12 -0800

    style email confirm page

--- client/src/screens/Auth/EmailConfirm/index.js
+++ client/src/screens/Auth/EmailConfirm/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import Title from '../../../components/Auth/title';
+import styled from 'styled-components';
 import { colors, breakpoints, fieldStyles } from '../../../styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${colors.gray50};
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: ${breakpoints.small}) {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+  @media (min-width: ${breakpoints.large}) {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  max-width: 36rem;
+  text-align: center;
+`;
 
 const EmailConfirm = () => {
   return (
-    <div>
-      <div>We have Sent you an email confirmation please check your inbox to continue</div>
-    </div>
+    <Wrapper>
+      <TitleWrapper>
+        <Title>We have Sent you an email confirmation please check your inbox to continue</Title>
+      </TitleWrapper>
+    </Wrapper>
   );
 };
 
