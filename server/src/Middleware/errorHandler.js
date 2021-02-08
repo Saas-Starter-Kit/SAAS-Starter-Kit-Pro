09b61f8b53293cedbb380b8c0cc63be50a7ec6c1
iqbal125 <iqbal125@yahoo.com>
2021-02-08 13:42:26 -0800

    fix errorhandler response

--- server/src/Middleware/errorHandler.js
+++ server/src/Middleware/errorHandler.js
@@ -1,6 +1,5 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  let message = 'Request Failed Try Again or Contact Support';
-  res.status(500).send({ message });
+  res.status(500).send({ error });
 };
