747108675fbca8865a2e422c57baeb9bc8ae4b26
gj315 <k1424sap@gmail.com>
2021-02-12 13:49:08 +0530

    Added all the todas functions to mongo support

--- server/src/Model/mongo/todos/todos.js
+++ server/src/Model/mongo/todos/todos.js
@@ -1,19 +1,27 @@
+import mongoose from 'mongoose';
+const objectId = mongoose.Types.ObjectId;
 import { Todos } from '../../../Database/mongo/models.js';
 
 export const postTodoModel = async (title, description, author, app_id) => {
-  let todo = new Todos({ title, description, author, app_id });
-  todo.save();
+  let todo = new Todos({ title, description, author, app_id: objectId(app_id) });
+  await todo.save();
+  return todo
 };
 
 export const getTodosModel = async (app_id) => {
-  let todos = await Todos.find({ app_id });
-  return todos;
+  return await Todos.find({ app_id });
 };
 
 export const putTodoModel = async (title, description, author, todo_id) => {
-  Todos.findByIdAndUpdate(todo_id, { title, author, description });
+  try {
+    return await Todos.findByIdAndUpdate(todo_id,
+      { $set: { title, author, description } }, {useFindAndModify: false});
+  } catch(e) {
+    throw new Error(e)
+  }
+  
 };
 
 export const deleteTodoModel = async (todo_id) => {
-  Todos.findByIdAndDelete(todo_id);
+  return await Todos.findByIdAndDelete(todo_id);
 };
--- server/src/Services/todos/todos.js
+++ server/src/Services/todos/todos.js
@@ -5,12 +5,16 @@ import {
   deleteTodoModel
 } from '../../Model/mongo/todos/todos.js';
 
-export const getTodos = async (req, res) => {
-  let app_id = req.query.app_id;
+export const getTodos = async (req, res, next) => {
+  try {
+    let app_id = req.query.app_id;
 
-  let result = await getTodosModel(app_id);
+    let result = await getTodosModel(app_id);
 
-  res.status(200).send(result);
+    res.status(200).send(result);
+  } catch (e) {
+    next(e)
+  }
 };
 
 export const postTodo = async (req, res) => {
@@ -24,21 +28,30 @@ export const postTodo = async (req, res) => {
   res.status(200).send('Post Successful');
 };
 
-export const putTodo = async (req, res) => {
-  let title = req.body.title;
-  let description = req.body.description;
-  let author = req.body.author;
-  let todo_id = req.body.todo_id;
+export const putTodo = async (req, res, next) => {
+  try {
+    let title = req.body.title;
+    let description = req.body.description;
+    let author = req.body.author;
+    let todo_id = req.body.todo_id;
 
-  await putTodoModel(title, description, author, todo_id);
+    await putTodoModel(title, description, author, todo_id);
 
-  res.status(200).send('Put Successful');
+    res.status(200).send('Put Successful');
+  } catch (e) {
+    next(e)
+  }
+
 };
 
-export const deleteTodo = async (req, res) => {
-  let todo_id = req.body.todo_id;
+export const deleteTodo = async (req, res, next) => {
+  try {
+    let todo_id = req.body.todo_id;
 
-  await deleteTodoModel(todo_id);
+    await deleteTodoModel(todo_id);
 
-  res.status(200).send('Delete Successful');
+    res.status(200).send('Delete Successful');
+  } catch (e) {
+    next(e)
+  }
 };
