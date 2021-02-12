6b9f468f29b72022b367061b2415808ac94e55cd
gj315 <k1424sap@gmail.com>
2021-02-12 10:54:48 +0530

    Added changes

--- server/src/Model/mongo/auth/authentication.js
+++ server/src/Model/mongo/auth/authentication.js
@@ -1,20 +1,22 @@
 import { Users } from '../../../Database/mongo/models.js';
 
 export const getUser = async (email) => {
-  let queryResult = Users.findOne({ email });
+  let queryResult = await Users.findOne({ email });
   return queryResult;
 };
 
 export const saveUsertoDB = async (email, username, firebaseId) => {
   let user = new Users({ email, username, fiebase_user_id: firebaseId });
-  let id = user.save();
+  let id = await user.save();
   return id;
 };
 
 export const updateUsernameModel = async (username, id) => {
-  Users.findByIdAndUpdate(id, { username });
+  const updatedRes = await Users.findByIdAndUpdate(id, { username });
+  return updatedRes;
 };
 
 export const updateEmailModel = async (email, id) => {
-  Users.findByIdAndUpdate(id, { email });
+  const updatedRes = await Users.findByIdAndUpdate(id, { email });
+  return updatedRes;
 };
