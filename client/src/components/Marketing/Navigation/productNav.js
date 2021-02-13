3d000f9a926c0a189e6f15262d221192cf59e4d5
iqbal125 <iqbal125@yahoo.com>
2021-02-12 18:34:50 -0800

    style product nav

--- client/src/components/Marketing/Navigation/productNav.js
+++ client/src/components/Marketing/Navigation/productNav.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: 2rem;
+`;
 
 const ProductNav = () => {
   return (
-    <div>
-      <Link to="/product/page1">Page1</Link>
-      <Link to="/product/page2">Page2</Link>
-      <Link to="/product/page3">Page3</Link>
-    </div>
+    <Wrapper>
+      <h1>Product</h1>
+      <Link
+        className="header_link_settings"
+        activeClassName="header_link_settings_active"
+        to="/product/page1"
+      >
+        Page1
+      </Link>
+      <Link
+        className="header_link_settings"
+        activeClassName="header_link_settings_active"
+        to="/product/page2"
+      >
+        Page2
+      </Link>
+      <Link
+        className="header_link_settings"
+        activeClassName="header_link_settings_active"
+        to="/product/page3"
+      >
+        Page3
+      </Link>
+    </Wrapper>
   );
 };
 
