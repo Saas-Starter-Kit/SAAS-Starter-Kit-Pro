8698f88206e8527d5c2ea93c6a773df997bc7f7e
iqbal125 <iqbal125@yahoo.com>
2021-02-22 19:48:20 -0800

    fix buildspec file

--- infrastructure/AWS CICD Pipeline/lib/aws_cicd_stack.ts
+++ infrastructure/AWS CICD Pipeline/lib/aws_cicd_stack.ts
@@ -1,15 +1,17 @@
-import * as cdk from "@aws-cdk/core"
-import * as ec2 from "@aws-cdk/aws-ec2"
-import * as codepipeline from "@aws-cdk/aws-codepipeline"
-import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions"
-import * as codebuild from "@aws-cdk/aws-codebuild"
-import * as secretsmanager from "@aws-cdk/aws-secretsmanager"
-import * as iam from "@aws-cdk/aws-iam"
-import * as s3 from "@aws-cdk/aws-s3"
-import * as cloudfront from "@aws-cdk/aws-cloudfront"
-import * as origins from "@aws-cdk/aws-cloudfront-origins"
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
+import * as iam from '@aws-cdk/aws-iam';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import { DockerIgnoreStrategy } from '@aws-cdk/core';
+import { BuildEnvironmentVariableType } from '@aws-cdk/aws-codebuild';
 
-require("dotenv").config()
+require('dotenv').config();
 
 //1. Generate Github Access token
 //2. Set the Token AWS secrets manager console.
@@ -17,117 +19,90 @@ require("dotenv").config()
 
 export class CICDStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
+    super(scope, id, props);
 
     //create vpc
-    const vpc = new ec2.Vpc(this, "VPC", {
+    const vpc = new ec2.Vpc(this, 'VPC', {
       natGateways: 0,
-      maxAzs: 3,
-    })
+      maxAzs: 3
+    });
 
     //Get Github access token
-    const secretGithub = secretsmanager.Secret.fromSecretAttributes(this, "ImportedSecret", {
-      secretArn: `arn:aws:secretsmanager:us-east-1:867137601660:secret:/github-access-token-NuotsR`,
-    })
-    const roleCodeBuild = new iam.Role(this, "CodeBuildRole", {
-      assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
-    })
+    const secretGithub = secretsmanager.Secret.fromSecretAttributes(this, 'ImportedSecret', {
+      secretArn: `arn:aws:secretsmanager:us-east-1:867137601660:secret:/github-access-token-NuotsR`
+    });
+    const roleCodeBuild = new iam.Role(this, 'CodeBuildRole', {
+      assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com')
+    });
 
-    secretGithub.grantRead(roleCodeBuild)
+    secretGithub.grantRead(roleCodeBuild);
 
     // Source Stage with github
-    const owner = process.env.GITHUB_OWNER ? process.env.GITHUB_OWNER : "GithubOwner"
-    const repo = process.env.GITHUB_REPO ? process.env.GITHUB_REPO : "Repo"
-    const oauthToken = secretGithub.secretValueFromJson("GITHUB_ACCESS_TOKEN")
+    const owner = process.env.GITHUB_OWNER ? process.env.GITHUB_OWNER : 'GithubOwner';
+    const repo = process.env.GITHUB_REPO ? process.env.GITHUB_REPO : 'Repo';
+    const oauthToken = secretGithub.secretValueFromJson('GITHUB_ACCESS_TOKEN');
 
-    const sourceOutput = new codepipeline.Artifact()
+    const sourceOutput = new codepipeline.Artifact();
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: "GitHub_Source",
+      actionName: 'GitHub_Source',
       owner,
       repo,
       oauthToken,
-      branch: "main",
-      output: sourceOutput,
-    })
+      branch: 'main',
+      output: sourceOutput
+    });
 
-    //Build Stage
-    const buildSpecObj = {
-      version: "0.2",
-      phases: {
-        install: {
-          "runtime-versions": {
-            nodejs: 14,
-          },
-        },
-        pre_build: {
-          commands: ['echo "--------PREBUILD PHASE--------"', "npm install"],
-        },
-        build: {
-          commands: ["npm run build"],
-        },
-      },
-      artifacts: {
-        files: ["build/*"],
-        basedirectory: "build",
-      },
-    }
+    const project = new codebuild.PipelineProject(this, 'MyProject', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('client/buildspec.yml'),
+      environmentVariables: {
+        key1: { value: 'Value1' }
+      }
+    });
 
-    // const buildSpecObj2 = {
-    //  version: "0.2",
-    //  phases: {
-    //    install: {
-    //      "runtime-versions": {
-    //        nodejs: 10,
-    //      },
-    //    },
-    //    pre_build: {
-    //      commands: ['echo "--------PREBUILD PHASE--------"', "cd client", "npm install"],
-    //    },
-    //    build: {
-    //      commands: ["npm run build", "npm run export"],
-    //    },
-    //  },
-
-    const project = new codebuild.PipelineProject(this, "MyProject", {
-      buildSpec: codebuild.BuildSpec.fromObject(buildSpecObj),
-    })
-
-    const buildOutput = new codepipeline.Artifact()
+    const buildOutput = new codepipeline.Artifact();
     const buildAction = new codepipeline_actions.CodeBuildAction({
-      actionName: "CodeBuild",
+      actionName: 'CodeBuild',
       project,
       input: sourceOutput,
-      outputs: [buildOutput],
-    })
+      outputs: [buildOutput]
+    });
 
     //deploy stage
-    const outputBucket = new s3.Bucket(this, "BuildOutputBucket", {})
+    const cors_rules: [s3.CorsRule] = [
+      { allowedOrigins: ['*'], allowedMethods: [s3.HttpMethods.GET] }
+    ];
+    const outputBucket = new s3.Bucket(this, 'BuildOutputBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      //removalPolicy: cdk.RemovalPolicy.DESTROY,
+      cors: cors_rules
+    });
     const deployAction = new codepipeline_actions.S3DeployAction({
-      actionName: "S3Deploy",
+      actionName: 'S3Deploy',
       bucket: outputBucket,
-      input: buildOutput,
-    })
+      input: buildOutput
+    });
 
-    new cloudfront.Distribution(this, "myDist", {
-      defaultBehavior: {origin: new origins.S3Origin(outputBucket)},
-    })
+    new cloudfront.Distribution(this, 'myDist', {
+      defaultBehavior: { origin: new origins.S3Origin(outputBucket) }
+    });
 
     //complete pipeline
-    new codepipeline.Pipeline(this, "MyPipeline", {
+    new codepipeline.Pipeline(this, 'MyPipeline', {
       stages: [
         {
-          stageName: "Source",
-          actions: [sourceAction],
+          stageName: 'Source',
+          actions: [sourceAction]
         },
         {
-          stageName: "Build",
-          actions: [buildAction],
+          stageName: 'Build',
+          actions: [buildAction]
         },
         {
-          stageName: "Deploy",
-          actions: [deployAction],
-        },
-      ],
-    })
+          stageName: 'Deploy',
+          actions: [deployAction]
+        }
+      ]
+    });
   }
 }
