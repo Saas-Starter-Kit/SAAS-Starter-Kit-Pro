91707c18b8df953d4cc5551303ba38c5e1419d53
iqbal125 <iqbal125@yahoo.com>
2021-02-24 11:23:39 -0800

    add comment to tests

--- server/src/Tests/fixtures/setUpEnvVars.js
+++ server/src/Tests/fixtures/setUpEnvVars.js
@@ -1,13 +1,13 @@
 //Env variables for tests
 
 //Postgres credentials,
-//Make a seperate db for tests,
-//These credentials are not mocked
+//A seperate db for tests is recommended,
+//These credentials and the database are not mocked
 process.env.DB_PASSWORD = 'password';
 process.env.DB_USER = 'postgres';
 process.env.DB_HOST = 'localhost';
 process.env.DB_NAME = 'testdb';
 process.env.DB_PORT = 5432;
 
-//user set auth secret
+//user set auth secret, mocked
 process.env.AUTH_SECRET = 'Secret';
