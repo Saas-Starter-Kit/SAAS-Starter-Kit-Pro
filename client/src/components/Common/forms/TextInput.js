4b939f52e7c5d75105761679038d4dfa2fc28c66
iqbal125 <iqbal125@yahoo.com>
2021-02-25 18:38:33 -0800

    update dependencies

--- client/src/components/Common/forms/TextInput.js
+++ client/src/components/Common/forms/TextInput.js
@@ -1,27 +1,25 @@
 import styled from 'styled-components';
-
-//const TextInput = styled.input`
-//  background-color: #ffffff;
-//  border-color: #d2d6dc;
-//  border-width: 1px;
-//  border-radius: 0.375rem;
-//  padding: 0.5rem 0.75rem 0.5rem 0.75rem;
-//  font-size: 1rem;
-//  line-height: 1.5;
-//  margin-top: 0.25rem;
-//  display: block;
-//  width: 100%;
-//  line-height: 1.25rem;
-//`;
+import { colors } from '../../../styles/theme';
 
 const TextInput = styled.input`
-  height: 34px;
+  background-color: #ffffff;
+  border-color: #d2d6dc;
+  border-width: 1px;
+  border-radius: 0.375rem;
+  padding: 0.5rem 0.75rem 0.5rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  margin-top: 0.25rem;
+  display: block;
   width: 100%;
-  border-radius: 3px;
-  border: 1px solid transparent;
-  border-top: none;
-  border-bottom: 1px solid #ddd;
-  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.39), 0 -1px 1px #fff, 0 1px 0 #fff;
+  line-height: 1.25rem;
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:focus {
+    box-shadow: 0 0 0 4px lightblue;
+    outline-width: 2px;
+    outline-color: lightblue;
+  }
 `;
 
 export default TextInput;
