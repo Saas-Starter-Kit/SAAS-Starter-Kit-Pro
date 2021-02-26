d41c2172cdaba5d72b2fdf0c037292da81f75cee
iqbal125 <iqbal125@yahoo.com>
2021-02-25 18:53:29 -0800

    comment out algolia

--- client/src/components/Common/forms/TextArea.js
+++ client/src/components/Common/forms/TextArea.js
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
-import { fieldStyles } from '../../../styles/theme';
 
 const TextArea = styled.textarea`
-  ${fieldStyles}
+  background-color: #ffffff;
+  border-color: #d2d6dc;
+  border-width: 1px;
+  border-radius: 0.375rem;
+  padding: 0.5rem 0.75rem 0.5rem 0.75rem;
+  font-size: 1rem;
+  margin-top: 0.25rem;
+  display: block;
+  width: 100%;
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:focus {
+    box-shadow: 0 0 0 4px lightblue;
+    outline-width: 2px;
+    outline-color: lightblue;
+  }
 `;
 
 export default TextArea;
