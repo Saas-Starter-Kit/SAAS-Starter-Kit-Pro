33e3a842971bba01698de2ca5cb8b4174f579a67
iqbal125 <iqbal125@yahoo.com>
2021-03-01 13:27:10 -0800

    fix transporter

--- server/src/Config/email.js
+++ server/src/Config/email.js
@@ -5,29 +5,32 @@ import nodemailer from 'nodemailer';
   Use mailtrap for testing and switch to 
   sendinblue when going live.
 */
+let transport;
 
-const prodTransporter = nodemailer.createTransport({
-  service: 'SendinBlue',
-  auth: {
-    user: process.env.SendInBlue_User,
-    pass: process.env.SendInBlue_Password
-  }
-});
-
-const devTransporter = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  //host: 'localhost', //will cause error but show email preview
-  port: 465,
-  secure: false,
-  auth: {
-    user: process.env.MAIL_TRAP_USERNAME,
-    pass: process.env.MAIL_TRAP_PASSWORD
-  }
-});
+if (process.env.NODE_ENV === 'development') {
+  transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    //host: 'localhost', //will cause error but show email preview
+    port: 465,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_TRAP_USERNAME,
+      pass: process.env.MAIL_TRAP_PASSWORD
+    }
+  });
+} else {
+  transport = nodemailer.createTransport({
+    service: 'SendinBlue',
+    auth: {
+      user: process.env.SendInBlue_User,
+      pass: process.env.SendInBlue_Password
+    }
+  });
+}
 
 export const email = new Email({
   send: true,
-  transport: devTransporter, // change to prodTransporter when going live
+  transport,
   preview: true, // to preview emails in your own browser
   views: {
     options: {
