4aee24b7e1752e9d8d7751728248ec740b182b87
iqbal125 <iqbal125@yahoo.com>
2021-03-22 17:40:13 -0700

    add mongo connect confirm

--- server/src/Database/mongo/db.js
+++ server/src/Database/mongo/db.js
@@ -8,4 +8,11 @@ let db = mongoose.connect(mongoURL, {
   useUnifiedTopology: true
 });
 
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+});
+
 export default db;
